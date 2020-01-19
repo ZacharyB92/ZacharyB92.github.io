c77afa5ec61d4e9858fb02c3c9811322ed9a0ee3
Zachary Blunt <blu18001@byui.edu>
2020-01-18 18:38:25 -0700

    updtae

--- scripts/currentDate.js
+++ scripts/currentDate.js
@@ -3,3 +3,9 @@ const currentDate = new Date();
 const currentYear = currentDate.getFullYear();
  console.log(currentYear);
 document.getElementById("currentYear").innerHTML = currentYear;
+
+var string = document.lastModified;
+var lastUp = new Date(document.lastModified);
+
+document.getElementById("lastUpdate").innerHTML = lastUp;
+console.log(lastUp)
