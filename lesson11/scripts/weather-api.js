6c6696a273595ab40455baadfb0b562ac24ebafa
Zachary Blunt <blu18001@byui.edu>
2020-03-21 11:07:23 -0600

    Changes

---
+++ lesson11/scripts/weather-api.js
@@ -0,0 +1,32 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=a401ede909567530b4dc74a0420779f5&units=imperial'
+
+fetch(apiURL)
+    .then(
+        response => {
+            return response.json();
+        }   
+     )
+     .then(
+         jsObject => {
+             console.log(jsObject);
+         
+             document.querySelector('#current-temp')
+                .textContent = jsObject.main.temp;
+            let image = 
+                'https://openweathermap.org/img/w/' + 
+                jsObject.weather[0].icon +      
+                '.png'
+
+            document.querySelector('#weather-icon')
+                .setAttribute('src', image);
+
+            let alt = jsObject.weather[0].description;
+
+            document.querySelector('#weather-icon')
+                .setAttribute('alt', alt);
+
+            // let weatherIcon = document.querySelector('#weather-icon');
+            // weatherIcon.setAttribute('src', image);
+            // weatherIcon.setAttribute('alt', alt);
+         }
+     );
